195b9c09cd5ec66c35bd2daa0f1e0291012b2bdf
Luka <luka@arthaus.info>
2021-09-09 23:50:21 +0200

    added more fonts, updated design, added the next image to mint

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 import { ethers } from "./js/ethers-5.2.esm.min.js";
 
-var provider, signer, address, smartContract, cost, connected, uriList
+var provider, signer, address, smartContract, cost, connected, uriList, supply, revealed
+
+// base url for pulling imgs
+const baseUrl = "https://nft.derschwarzejugo.com/schnabeltiere/metadata/Fat%20Plat%20%23"
+
 // smart contract
-const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+const contractAddress = "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f"
 
 // declared functions which you want to use of the contract (not necessairly all)
 const contractAbi = [
 	"function mint(address _to, uint256 _mintAmount) public payable",
 	"function tokenURI(uint256 tokenId)	public view	returns (string)",
 	"function walletOfOwner(address _owner)	public view returns (uint256[])",
-	"function cost() public view returns (uint256)"
+	"function cost() public view returns (uint256)",
+	"function totalSupply() public view returns (uint256)"
 ]
 
 let connectInterval = setInterval(() => {
@@ -25,7 +30,7 @@ let connectInterval = setInterval(() => {
 // listeners
 $(window).on("load", async () => {
 	if (!window.ethereum) {
-		$('#status').html('Please install Metamask!!')
+		$('#status').html('Please install or allow Metamask!!')
 	} else {
 		provider = new ethers.providers.Web3Provider(window.ethereum)
 		signer = provider.getSigner()
@@ -64,7 +69,7 @@ $("#connectMetamask").on("click", async () => {
 			console.log(error)
 		}
 	} else {
-		$('#status').html('Please install Metamask!!')
+		$('#status').html('Please install or allow Metamask!!')
 		connected = false
 	}
 })
@@ -83,55 +88,128 @@ $("#claimNFT").on("click", async () => {
 	}
 })
 
+$("#loadMore").on("click", async () => {
+	let interval = revealed + 20
+	for (revealed; revealed < interval; revealed++) {
+		let element = baseUrl + revealed + ".json"
+		createAppendImg(element, $(".mintedNftsWrapper"), true)
+	}	
+})
+
 
 // gets run when metamask is connected
 const init = () => {
 	smartContract = new ethers.Contract(contractAddress, contractAbi, provider)
-	getAddress().then(() => getOwnWallet())
+	getAddress()
+		.then(() => getOwnWallet())
+		.then(() => getSupply())
+		.then(() => getImgToMint())
 	getCost()
 }
 
 async function getAddress () {
-	address = await signer.getAddress()
+	try {
+		address = await signer.getAddress()
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 async function getCost () {
-
-	cost = ethers.utils.formatEther(await smartContract.cost())}
-
-// get holdings of walletowner, create links and output images
-async function getOwnWallet (appended = false) {
-	const walletOfOwner = await smartContract.connect(signer).walletOfOwner(address)
-	uriList = []
-	for (let i = 0; i <= walletOfOwner.length - 1; i++) {
-		uriList[i] = await smartContract.connect(signer).tokenURI(walletOfOwner[i])
+	try {
+		cost = ethers.utils.formatEther(await smartContract.cost())
+		let innerText = "Current price: " + cost + ethers.constants.EtherSymbol
+		$("#nftCost").text(innerText)
+	} catch (error) {
+		console.log(error)
 	}
-	if (!appended) {
-		uriList.forEach(element => {
-			createAppendImg(element)	
-		});
-	} else {
-		let imgCollection = $(".ownNftsWrapper figure img") 
-		let expectedEl = imgCollection[imgCollection.length - appended]
-		let expectedImg = $(expectedEl).last().attr("src")
-		let currentImg = uriList[uriList.length - appended].replace("metadata", "images").replace(".json", ".png")
-		// checking if newest nft already has been loaded
-		if (expectedImg == currentImg) {
-			setTimeout(() => {
-				getOwnWallet(appended)
-			}, 1000);
-		} else {
-			// if not false, appended equals amount of minted elements
-			for (appended; appended > 0; appended--) {
-				createAppendImg(uriList[uriList.length - appended])
-			}
+}
+
+async function getImgToMint() {
+	if ($(".ownNfts").length > 0) {
+		try {
+			let innerSupply = await smartContract.connect(address).totalSupply()
+			let element = baseUrl + innerSupply + ".png"
+			let split = element.split("/")
+			let name = decodeURIComponent(split[split.length - 1].replace(".png", ""))
+			let img = "<figure><img src='" + element.replace("metadata", "images") + "' title='" + name + "' alt='Image of NFT " + name + "' /><figcaption>Claim your " + name + " right now!</figcaption></figure>"
+			$("#nftCost").parent().prepend(img)	
+		} catch (error) {
+			console.log(error)	
 		}
 	}
 }
 
-function createAppendImg(element) {
+async function getSupply() {
+	if ($(".mintedNfts").length > 0) {
+		try {
+			supply = await smartContract.connect(address).totalSupply()
+			supply = supply.toNumber()
+			for (let i = 0; i < supply; i++) {
+				let element = baseUrl + i + ".json"
+				createAppendImg(element, $(".mintedNftsWrapper"))
+			}
+			setTimeout(() => {
+				if (!revealed > 0) {
+					revealed = supply
+				}
+				let interval = revealed + 20
+				for (revealed; revealed < interval; revealed++) {
+					let element = baseUrl + revealed + ".json"
+					createAppendImg(element, $(".mintedNftsWrapper"), true)
+				}
+			}, 1000);
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
+
+
+// get holdings of walletowner, create links and output images
+async function getOwnWallet (appended = false) {
+	if ($(".ownNfts").length > 0) {
+		try {
+			const walletOfOwner = await smartContract.connect(signer).walletOfOwner(address)
+			uriList = []
+			for (let i = 0; i <= walletOfOwner.length - 1; i++) {
+				try {
+					uriList[i] = await smartContract.connect(signer).tokenURI(walletOfOwner[i])
+				} catch (error) {
+					console.log(error)
+				}
+			}
+			if (!appended) {
+				uriList.forEach(element => {
+					createAppendImg(element, $(".ownNftsWrapper"))	
+				});
+			} else {
+				let imgCollection = $(".ownNftsWrapper figure img") 
+				let expectedEl = imgCollection[imgCollection.length - appended]
+				let expectedImg = $(expectedEl).last().attr("src")
+				let currentImg = uriList[uriList.length - appended].replace("metadata", "images").replace(".json", ".png")
+				// checking if newest nft already has been loaded
+				if (expectedImg == currentImg) {
+					setTimeout(() => {
+						getOwnWallet(appended)
+					}, 1000);
+				} else {
+					// if not false, appended equals amount of minted elements
+					for (appended; appended > 0; appended--) {
+						createAppendImg(uriList[uriList.length - appended], $(".ownNftsWrapper"))
+					}
+				}
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
+
+function createAppendImg(element, wrapper, filtered = false) {
 	let split = element.split("/")
 	let name = decodeURIComponent(split[split.length - 1].split(".json")[0])
-	let img = "<figure><img src='" + element.replace("metadata", "images").replace(".json", ".png") + "' title='" + name + "' alt='Image of NFT " + name + "' /><figcaption>" + name + "</figcaption></figure>"
-	$(".ownNftsWrapper").append(img)
+	let img = "<figure><img " + (filtered ? "class='filtered'" : "") + " src='" + element.replace("metadata", "images").replace(".json", ".png") + "' title='" + name + "' alt='Image of NFT " + name + "' /><figcaption>" + name + "</figcaption></figure>"
+	wrapper.append(img)
 }
